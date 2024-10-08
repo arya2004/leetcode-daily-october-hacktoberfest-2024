938928375a0559ea105862761375f3f334b6cf63
Mohd-FaiZ-Jr <mohdfaizofficial487@gmail.com>
2024-10-08 17:52:51 +0530

    Add Day 28 solution in Java

--- solutions/day28/solution_java.java
+++ solutions/day28/solution_java.java
@@ -7,14 +7,14 @@ public class solution_java{
     }
 
     public static int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length-1;
-        while(left <= right){
-            int total = numbers[left] + numbers[right];
-            if(total == target) break;
-            if(total < target) left++;
-            else right--;
+        int left = 0;   //taking a left pointer to move forward.
+        int right = numbers.length-1;   //taking a right pointer to move backward.
+        while(left <= right){   //condition till it is true..
+            int total = numbers[left] + numbers[right];     //checking the sum of the two numbers at index left & right.
+            if(total == target) break;      //if founds equals to the target number, then break the loop.
+            if(total < target) left++;      //checks if total is less than the target. If yes, it means that left index value is smaller. So incremented it.
+            else right--;           //otherwise total is bigger, which means we need to get a smaller number. Hence decrement right index by one.
         }
-        return new int[] {left+1, right+1};
+        return new int[] {left+1, right+1};     //returns the final two indices which is 1-indexed
     }
 }
